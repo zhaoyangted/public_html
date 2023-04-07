9e8d5c31d347b00cd615c506465f5837054ebab9
zhaoyangted <zhaoyangted@gmail.com>
2023-04-07 11:54:06 +0800

    revive google scripts

--- application/views/front/_header.php
+++ application/views/front/_header.php
@@ -53,26 +53,26 @@ $WebConfigData=$this->webmodel->GetWebData();
     <link rel="stylesheet" href="<? echo CCODE::DemoPrefix.('/css/front/nav-layout.css')?>">
     <!--//mobile menu -->
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <!-- <script async src="https://www.googletagmanager.com/gtag/js?id=UA-50600153-2"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-50600153-2"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
       gtag('config', 'UA-50600153-2');
-    </script> -->
+    </script>
     <!-- Google Tag Manager -->
-    <!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-TVM3JRC');</script> -->
+    })(window,document,'script','dataLayer','GTM-TVM3JRC');</script>
     <!-- End Google Tag Manager -->
   </head>
 <body>
   <!-- Google Tag Manager (noscript) -->
-  <!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVM3JRC"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVM3JRC"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
 <a href="#"><div class="gotoTOP">TOP</div></a>
 <header>
